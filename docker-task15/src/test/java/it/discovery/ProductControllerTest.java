e20f03a8a34a552bdf1d5e4f44d2131451efcad8
Sergiy <sergey.morenets@gmail.com>
2022-10-23 16:02:40 +0300

    Task 16. Zonky

--- docker-task15/src/test/java/it/discovery/ProductControllerTest.java
+++ docker-task15/src/test/java/it/discovery/ProductControllerTest.java
@@ -1,5 +1,6 @@
 package it.discovery;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import it.discovery.model.Product;
 import it.discovery.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
@@ -7,29 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Testcontainers
+//@Testcontainers
 @AutoConfigureDataJpa
+@AutoConfigureEmbeddedDatabase
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 public class ProductControllerTest {
 
-	@Container
-	static MySQLContainer mysql = new MySQLContainer<>("mysql:8")
-			.withExposedPorts(3306).withReuse(true);
+//	@Container
+//	static MySQLContainer mysql = new MySQLContainer<>("mysql:8")
+//			.withExposedPorts(3306).withReuse(true);
 
-	@DynamicPropertySource
-	static void mysqlProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url",
-				() -> String.format("jdbc:mysql://localhost:%s/test",
-						mysql.getMappedPort(3306)));
-	}
+//	@DynamicPropertySource
+//	static void mysqlProperties(DynamicPropertyRegistry registry) {
+//		registry.add("spring.datasource.url",
+//				() -> String.format("jdbc:mysql://localhost:%s/test",
+//						mysql.getMappedPort(3306)));
+//	}
 
 	@Autowired
 	ProductRepository productRepository;
