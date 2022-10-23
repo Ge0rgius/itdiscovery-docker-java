90f7f6897b677fa00b06dbc5ad8f033d1138c3ee
Sergiy <sergey.morenets@gmail.com>
2022-10-23 12:08:40 +0300

    Task 12. Use environment variable to customize project settings

--- docker-task12/client/src/main/java/it/discovery/web/ClientController.java
+++ docker-task12/client/src/main/java/it/discovery/web/ClientController.java
@@ -1,5 +1,6 @@
 package it.discovery.web;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,15 @@ import org.springframework.web.client.RestTemplate;
 public class ClientController {
     private final RestTemplate restTemplate = new RestTemplate();
 
+    private final Environment env;
+
+    public ClientController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("greeting/{name}")
     public String hi(@PathVariable String name) {
-        return restTemplate.getForObject("http://server:9000/greeting/" + name,
+        return restTemplate.getForObject(env.getRequiredProperty("server.url") + name,
                 String.class);
     }
 }
